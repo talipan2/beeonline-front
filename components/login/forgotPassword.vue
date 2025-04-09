25381588731094263047c3a226923a9ff04ccda1
Denis Valitov <ssilverufa@yandex.ru>
2025-04-09 12:49:36 +0500

    fix

--- components/login/forgotPassword.vue
+++ components/login/forgotPassword.vue
@@ -1,29 +1,57 @@
 <template>
   <div class="login">
     <h1 class="login__title">Восстановление пароля</h1>
-    <CommonAlerts alert="Мы отправили письмо со ссылкой для восстановления!" type="info" v-if="isSendedMailToReset" />
-    <p class="login__subtitle">Вы можете восстановить пароль, отправив свою электронную почту. На нее мы вышлем письмо с инструкцией по восстановлению.</p>
-    <Form class="forgot__form" @submit="handleSubmit">
-      <UiInput
-        :rules="{ required: true, min: 6, email: true }"
-        class="auth__input"
-        placeholder="E-mail"
-        v-model="email"
-        name="email"
-        label="E-mail"
-      />
-      <div class="forgot__btn-container">
-        <UiButton
-          to="/login"
-          class="forgot__btn forgot__btn_type-back"
-          variant="default"
-          >Вернуться к авторизации</UiButton
-        >
-        <UiButton type="submit" to="/login" class="forgot__btn" variant="quinary" size="large"
-          >Отправить</UiButton
-        >
-      </div>
-    </Form>
+    <template v-if="stage === 'email'">
+      <CommonAlerts alert="Мы отправили письмо со ссылкой для восстановления!" type="info" v-if="isSendedMailToReset" />
+      <p class="login__subtitle">Вы можете восстановить пароль, отправив свою электронную почту. На нее мы вышлем письмо с инструкцией по восстановлению.</p>
+      <Form class="forgot__form" @submit="handleSubmit">
+        <UiInput
+          :rules="{ required: true, min: 6, email: true }"
+          class="auth__input"
+          placeholder="E-mail"
+          v-model="email"
+          name="email"
+          label="E-mail"
+        />
+        <div class="forgot__btn-container">
+          <UiButton
+            to="/login"
+            class="forgot__btn forgot__btn_type-back"
+            variant="default"
+            >Вернуться к авторизации</UiButton
+          >
+          <UiButton type="submit" to="/login" class="forgot__btn" variant="quinary" size="large"
+            >Отправить</UiButton
+          >
+        </div>
+      </Form>
+    </template>
+    <template v-else>
+      <p class="login__subtitle">На ваш регистрационный email отправлен код. Введите его в поле ниже.</p>
+      <Form class="auth__form" @submit="handleSubmitPassword">
+        <UiInput
+          :rules="{ required: true, }"
+          class="auth__input"
+          placeholder="Код из письма"
+          v-model="password"
+          name="email-code"
+          label="Код из письма"
+          type="password"
+        />
+        <div class="auth__btn-container">
+          <UiButton to="/login" class="auth__btn" variant="tertiary" size="large"
+            >Войти с паролем</UiButton
+          >
+          <UiButton
+            type="submit"
+            class="auth__btn"
+            variant="quinary"
+            size="large"
+            >Отправить</UiButton
+          >
+        </div>
+      </Form>
+    </template>
   </div>
 </template>
 
@@ -33,13 +61,26 @@ import { useUserStore } from '~/store/userStore';
 const userStore = useUserStore();
 
 const email = ref('');
+const password = ref('')
 
+const stage = ref('email')
+const router = useRouter();
 const isSendedMailToReset = ref(false);
 
 const handleSubmit = (values, form) => {
   userStore.forgotPassword(values, form).then((res) => {
     isSendedMailToReset.value = true
+    stage.value = 'password'
   });
+}
+
+const handleSubmitPassword = (values, form) => {
+  userStore.authUser({
+    email: email.value,
+    password: password.value
+  }, form).then((res) => {
+    router.push({ path: '/desktop' });
+  })
 }
 
 </script>
