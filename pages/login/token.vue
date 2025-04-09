ad8951879148b1e38bae13bf5f5fb4705935ebcd
AlbertZaripov <AJIb6EPT@gmail.com>
2025-04-09 13:50:05 +0500

    token auth

---
+++ pages/login/token.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+
+import { useUserStore } from "~/store/userStore";
+
+const userStore = useUserStore();
+
+const route = useRoute();
+
+const token = route.query.token;
+const redirect = route.query.redirect;
+userStore.userToken = token;
+localStorage.setItem("token", token);
+userStore.checkAuth().then(() => {
+  if(redirect) {
+    // TODO: добавить в конфиги front_url, проверять его наличие, и если есть - external: false
+    navigateTo(redirect, { external: true });
+  }
+})
+</script>
+
+<style>
+
+</style>
