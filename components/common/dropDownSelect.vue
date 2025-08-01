1c753d53409ab8ef93e8aff4cf2fb8b746179954
Denis Valitov <ssilverufa@yandex.ru>
2025-08-01 11:48:34 +0500

    feat: новый селект для перевода

--- components/common/dropDownSelect.vue
+++ components/common/dropDownSelect.vue
@@ -1,43 +1,77 @@
 <template>
-	<div class="drop-down-select-wrapper">
-		<UiNewDropdown
-			:arrow="false"
-			:placement="'bottom-start'"
-			:offset="[0, 10]"
-			class="drop-down-select"
-			ref="dropdownRef"
+	{{ counter }}
+	<Field
+		:name="name"
+		:label="label"
+		:rules="rules"
+		ref="fieldRef"
+		v-slot="{ errors, meta }"
+		:class="$attrs.class"
+	>
+		<div
+			class="drop-down-select-wrapper"
+			:class="[
+				$attrs.class,
+				`drop-down-select-wrapper_variant_${variant}`,
+				{ invalid: errors.length && meta.touched },
+			]"
 		>
-			<UiButton
-				class="drop-down-select__dropdown-button"
-				variant="elevated"
+			{{ selectedOption }}
+			<UiNewDropdown
+				:arrow="false"
+				placement="bottom-start"
+				:offset="offset"
+				class="drop-down-select"
+				ref="dropdownRef"
 			>
-				<slot name="icon-left" />
-				{{ selectedOption.label }}
-				<SvgoDropDownSecondary
-					class="svg-m drop-down-select__dropdown-button-icon"
-					:class="{ active: isOpen }"
-				/>
-			</UiButton>
-			<template #content>
-				<div class="drop-down-select__dropdown-content">
-					<div class="drop-down-select__dropdown-list">
-						<UiButton
-							class="drop-down-select__dropdown-item"
-							v-for="option in options"
-							:key="option.id"
-							variant="default"
-							size="large"
-							type="button"
-							:without-padding="true"
-							@click="handleSelect(option)"
-						>
-							{{ option.label }}
-						</UiButton>
+				<UiButton
+					class="drop-down-select__dropdown-button"
+					variant="elevated"
+					:class="{
+						'drop-down-select__dropdown-button_placeholder':
+							selectedOption?.disabled,
+					}"
+				>
+					<slot name="icon-left" />
+					{{ selectedOption?.label }}
+					<SvgoDropDownSecondary
+						class="svg-m drop-down-select__dropdown-button-icon"
+						:class="{ active: isOpen }"
+					/>
+				</UiButton>
+				<template #content>
+					<div class="drop-down-select__dropdown-content">
+						<div class="drop-down-select__dropdown-list">
+							<UiButton
+								class="drop-down-select__dropdown-item"
+								v-for="option in options"
+								:key="option.id"
+								variant="default"
+								size="large"
+								type="button"
+								:without-padding="true"
+								@click="(event) => handleSelect(option, event)"
+								:disabled="option?.disabled"
+							>
+								{{ option.label }}
+							</UiButton>
+						</div>
 					</div>
-				</div>
-			</template>
-		</UiNewDropdown>
-	</div>
+				</template>
+			</UiNewDropdown>
+			<div
+				class="invalid-error"
+				v-if="errorShow"
+			>
+				<span
+					v-if="errors.length && meta.touched"
+					class="invalid-error__text"
+				>
+					{{ errors[0] }}
+				</span>
+			</div>
+		</div>
+	</Field>
 </template>
 
 <script setup>
@@ -50,34 +84,159 @@
 			type: Array,
 			default: () => [],
 		},
+		name: {
+			type: String,
+			default: '',
+		},
+		rules: {
+			type: [String, Object],
+			default: '',
+		},
+		errorShow: {
+			type: Boolean,
+			default: true,
+		},
+		label: {
+			type: String,
+			default: '',
+		},
+		variant: {
+			type: String,
+			default: 'default',
+			validator: (value) => ['default', 'elevated'].includes(value),
+		},
+		returnValue: {
+			type: Boolean,
+			default: false,
+		},
 	});
 
+	const fieldRef = ref(null);
 	const dropdownRef = ref(null);
+
+	const offset = computed(() => {
+		if (props.variant === 'elevated') {
+			return [0, 10];
+		}
+		return [0, 2];
+	});
 
 	const selectedOption = computed(() => {
 		if (!props.modelValue) return props.options[0];
-		return props.options.find((option) => option.value === props.modelValue);
+		if (props.returnValue) {
+			console.log(
+				props.options.find((option) => option.value === props.modelValue)
+			);
+			return props.options.find((option) => option.value === props.modelValue);
+		} else {
+			return props.options.find((option) => option.id === props.modelValue);
+		}
 	});
 
 	const emit = defineEmits(['update:modelValue']);
 
+	const counter = ref(0);
+
 	const handleSelect = (option) => {
-		emit('update:modelValue', option.value);
+		if (props.returnValue) {
+			emit('update:modelValue', option.value);
+		} else {
+			emit('update:modelValue', option.id);
+		}
+		if (fieldRef.value) {
+			fieldRef.value?.setValue(option.value);
+			fieldRef.value?.setTouched(true);
+		}
+		counter.value++;
 	};
 
 	const isOpen = computed(() => {
 		return dropdownRef.value?.tippy?.state?.isVisible;
 	});
+
+	watch(
+		() => isOpen.value,
+		(newVal, oldVal) => {
+			if (newVal === false && oldVal === true) {
+				fieldRef.value?.setTouched(true);
+				fieldRef.value?.validate();
+			}
+		}
+	);
+
+	const updateCounter = () => {
+		counter.value++;
+	};
+
+	// onMounted(() => {
+	// 	setInterval(updateCounter, 1000);
+	// });
 </script>
 
 <style lang="scss">
 	.drop-down-select-wrapper {
+		font-size: 1rem;
+		position: relative;
 		[data-tippy-root] {
 			box-shadow: 0px 4px 16px 0px rgba(0, 0, 0, 0.08);
 			border-radius: 1em;
 			overflow: hidden;
 			background-color: var(--color-white);
 			border: 1px solid var(--color-gray-200);
+			width: 100%;
+		}
+
+		&_variant_default {
+			[data-tippy-root] {
+				border-radius: 0;
+			}
+			.drop-down-select {
+				width: 100%;
+
+				&__dropdown-content {
+					padding: 1em 0;
+				}
+
+				&__dropdown-list {
+					row-gap: 0;
+				}
+
+				&__dropdown-button {
+					border-radius: 0;
+					border: 1px solid var(--border-color-secondary);
+					padding: 0.625em;
+					color: var(--text-color-senary);
+
+					svg {
+						width: 1.4em;
+						height: 1.4em;
+						path {
+							fill: #000;
+						}
+					}
+				}
+
+				&__dropdown-item {
+					border-radius: 0;
+					padding: 0.5em !important;
+					width: 100%;
+					justify-content: flex-start;
+					transition: none;
+
+					&:hover:not(:disabled) {
+						background-color: var(--primary-color);
+						color: var(--color-white);
+					}
+				}
+			}
+		}
+
+		&.invalid {
+			.drop-down-select {
+				&__dropdown-button {
+					border-color: var(--text-color-danger);
+				}
+			}
 		}
 	}
 
@@ -85,14 +244,19 @@
 		font-size: 1em;
 
 		&__dropdown-button {
-			font-size: 1.6em;
+			font-size: 1em;
 			font-weight: 400;
 			column-gap: 1em;
 			text-transform: none;
+			justify-content: space-between;
+
+			&_placeholder {
+				color: var(--color-gray-500);
+			}
 
 			svg {
-				width: 2.4rem;
-				height: 2.4rem;
+				width: 2.4em;
+				height: 2.4em;
 			}
 
 			&-icon.active {
