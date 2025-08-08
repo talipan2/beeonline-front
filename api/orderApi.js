80b9d95f26ea31ca2836a47637127b83a7cd135f
Denis Valitov <ssilverufa@yandex.ru>
2025-08-08 09:31:20 +0500

    feat: добавить опросник при закрытии заказа

--- api/orderApi.js
+++ api/orderApi.js
@@ -1,85 +1,100 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default {
-  async getOrders(params={}) {
-    return axios.get(`orders`, {params})
-  },
+	async getOrders(params = {}) {
+		return axios.get(`orders`, { params });
+	},
 
-  async getOrganizationOrder(id) {
-    return axios.get(`orders`)
-  },
+	async getOrganizationOrder(id) {
+		return axios.get(`orders`);
+	},
 
-  async getOrder(id) {
-    return axios.get(`orders/${id}`)
-  },
+	async getOrder(id) {
+		return axios.get(`orders/${id}`);
+	},
 
-  async setOrder(values, form) {
-    return useApi().post(`orders`, values, form)
-  },
+	async setOrder(values, form) {
+		return useApi().post(`orders`, values, form);
+	},
 
-  async editOrder(id, data, form) {
-    const {
-      name,
-      description,
-      termsOfCooperation,
-      rawMaterials,
-      price,
-      batch,
-      completionDate,
-      patterns,
-      cities,
-      regions,
-      countries,
-      isSafeDeal,
-      status,
-      categories,
-      gallery,
-      isAgreedOrderPlacement,
-      currency_id,
-    } = data
-    const params = {}
-    if(name !== '') params.name = name
-    if(description !== '') params.description = description
-    if(termsOfCooperation !== '') params.conditions = termsOfCooperation
-    if(rawMaterials !== '') params.material = rawMaterials
-    if(price !== '') params.price = price
-    if(batch !== '') params.batch = batch
-    if(patterns != null) params.pattern = Number(patterns)
-    if(completionDate !== '') params.deadline_at = completionDate
-    if(cities && cities.length > 0) params.cities = cities
-    if(regions && regions.length > 0) params.regions = regions
-    if(countries && countries.length > 0) params.countries = countries
-    if(isSafeDeal !== null) params.is_safedeal = isSafeDeal
-    if(status) params.status = status
-    if(categories && categories.length > 0) params.product_categories = categories
-    if(gallery && gallery.length > 0) params.gallery = gallery
-    if(isAgreedOrderPlacement !== null) params.tg_publish = isAgreedOrderPlacement
-    if(currency_id !== null) params.currency_id = currency_id
-    return useApi().patch(`orders/${id}`, params, form)
-  },
+	async editOrder(id, data, form) {
+		const {
+			name,
+			description,
+			termsOfCooperation,
+			rawMaterials,
+			price,
+			batch,
+			completionDate,
+			patterns,
+			cities,
+			regions,
+			countries,
+			isSafeDeal,
+			status,
+			categories,
+			gallery,
+			isAgreedOrderPlacement,
+			currency_id,
+		} = data;
+		const params = {};
+		if (name !== '') params.name = name;
+		if (description !== '') params.description = description;
+		if (termsOfCooperation !== '') params.conditions = termsOfCooperation;
+		if (rawMaterials !== '') params.material = rawMaterials;
+		if (price !== '') params.price = price;
+		if (batch !== '') params.batch = batch;
+		if (patterns != null) params.pattern = Number(patterns);
+		if (completionDate !== '') params.deadline_at = completionDate;
+		if (cities && cities.length > 0) params.cities = cities;
+		if (regions && regions.length > 0) params.regions = regions;
+		if (countries && countries.length > 0) params.countries = countries;
+		if (isSafeDeal !== null) params.is_safedeal = isSafeDeal;
+		if (status) params.status = status;
+		if (categories && categories.length > 0)
+			params.product_categories = categories;
+		if (gallery && gallery.length > 0) params.gallery = gallery;
+		if (isAgreedOrderPlacement !== null)
+			params.tg_publish = isAgreedOrderPlacement;
+		if (currency_id !== null) params.currency_id = currency_id;
+		return useApi().patch(`orders/${id}`, params, form);
+	},
 
-  async uploadLogo(id, data) {
-    return axios.post(`/orders/${id}/attach-logo`, {media_id: data})
-  },
+	async uploadLogo(id, data) {
+		return axios.post(`/orders/${id}/attach-logo`, { media_id: data });
+	},
 
-  async uploadGallery(id, data) {
-    return axios.post(`/orders/${id}/attach-gallery`, {media_ids: [...data]})
-  },
+	async uploadGallery(id, data) {
+		return axios.post(`/orders/${id}/attach-gallery`, { media_ids: [...data] });
+	},
 
-  async uploadTzFiles(id, data) {
-    return axios.post(`/orders/${id}/attach-tz-files`, {media_ids: [...data]})
-  },
+	async uploadTzFiles(id, data) {
+		return axios.post(`/orders/${id}/attach-tz-files`, {
+			media_ids: [...data],
+		});
+	},
 
-  async updateOrderStep(id, currentStep) {
-    return axios.patch(`orders/${id}/update-step`, {current_step: currentStep})
-  },
+	async updateOrderStep(id, currentStep) {
+		return axios.patch(`orders/${id}/update-step`, {
+			current_step: currentStep,
+		});
+	},
 
-  async getSelfOrders(organizationId, params={}) {
-    return axios.get(`organizations/${organizationId}/orders`, {params})
-  },
+	async getSelfOrders(organizationId, params = {}) {
+		return axios.get(`organizations/${organizationId}/orders`, { params });
+	},
 
-  async getPhoneNumber(id)
-  {
-    return useApi().get(`orders/${id}/phone-number`, null, null, true);
-  }
-}
+	async getPhoneNumber(id) {
+		return useApi().get(`orders/${id}/phone-number`, null, null, true);
+	},
+
+	// Снятие заказа с публикации
+	async orderUnpublish(id, values, form) {
+		return useApi().post(`orders/${id}/unpublish`, values, form);
+	},
+
+	// Получение исполнителей, с которыми были сообщения в чате
+	async getOrderPublicationsPerformers(id, params) {
+		return useApi().get(`order-publications/${id}/performers`, params);
+	},
+};
