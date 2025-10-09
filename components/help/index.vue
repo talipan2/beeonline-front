1c327be4c73a083079f689587f24204d5bac0b33
AlbertZaripov <AJIb6EPT@gmail.com>
2025-10-09 16:06:14 +0500

    schema

--- components/help/index.vue
+++ components/help/index.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="help-container">
-    <div class="help-container__left-side">
-      <div class="help-container__questions" :style="{ top: `${settingStore.headerHeight}px` }">
-        <div class="help-container__question" v-for="(chapter, index) in chapters" :key="index">
-          <UiButton type="button" class="help-container__question-btn"
-            :class="{'active': isActiveChapter === index}" variant="default" size="large"
-            @click="handleActiveChapter(index)">
-            {{chapter.name}}
-            <SvgoDropDownNew class="svg-m" />
-          </UiButton>
-          <ul class="help-container__question-list" v-if="isActiveChapter === index">
-            <li v-for="(question, indexQuestion) in chapter.items" :key="indexQuestion">
-              <UiButton type="button" class="link" variant="default" size="large"
-                :class="{'active': isActiveQuestion.question === indexQuestion && isActiveQuestion.chapter === index}"
-                @click="handleClickQuestion(indexQuestion)">
-                {{ question.question }}
-              </UiButton>
-            </li>
-          </ul>
+    <div class="help-container">
+        <div class="help-container__left-side">
+            <div class="help-container__questions" :style="{ top: `${settingStore.headerHeight}px` }">
+                <div class="help-container__question" v-for="(chapter, index) in chapters" :key="index">
+                    <UiButton type="button" class="help-container__question-btn"
+                        :class="{ 'active': isActiveChapter === index }" variant="default" size="large"
+                        @click="handleActiveChapter(index)">
+                        {{ chapter.name }}
+                        <SvgoDropDownNew class="svg-m" />
+                    </UiButton>
+                    <ul class="help-container__question-list" v-if="isActiveChapter === index">
+                        <li v-for="(question, indexQuestion) in chapter.items" :key="indexQuestion">
+                            <UiButton type="button" class="link" variant="default" size="large"
+                                :class="{ 'active': isActiveQuestion.question === indexQuestion && isActiveQuestion.chapter === index }"
+                                @click="handleClickQuestion(indexQuestion)">
+                                {{ question.question }}
+                            </UiButton>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="help-container__content" ref="helpContent">
-      <h4>{{ isActiveQuestion.title }}</h4>
-      <div v-html="isActiveQuestion.answer"></div>
-    </div>
+        <div class="help-container__content" ref="helpContent">
+            <h4>{{ isActiveQuestion.title }}</h4>
+            <div v-html="isActiveQuestion.answer"></div>
+        </div>
     </div>
 </template>
 
@@ -39,210 +39,253 @@ const offset = ref(null);
 
 const isActiveChapter = ref(0);
 const isActiveQuestion = ref({
-  chapter: 0,
-  question: 0,
-  answer: '',
-  title: '',
+    chapter: 0,
+    question: 0,
+    answer: '',
+    title: '',
 })
 
+// Функция для генерации JSON-LD schema
+const generateFAQSchema = () => {
+    if (!chapters.value || chapters.value.length === 0) return null;
 
+    const allQuestions = [];
+
+    chapters.value.forEach(chapter => {
+        if (chapter.items && Array.isArray(chapter.items)) {
+            chapter.items.forEach(item => {
+                if (item.question && item.answer) {
+                    // Очистка HTML-тегов из ответа для schema
+                    const cleanAnswer = item.answer.replace(/<[^>]*>/g, '').trim();
+
+                    allQuestions.push({
+                        "@type": "Question",
+                        "name": item.question,
+                        "acceptedAnswer": {
+                            "@type": "Answer",
+                            "text": cleanAnswer
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+    return {
+        "@context": "https://schema.org",
+        "@type": "FAQPage",
+        "mainEntity": allQuestions
+    };
+}
 
 const handleActiveChapter = (id) => {
-  if(isActiveChapter.value !== id) {
-    isActiveChapter.value = id
-  } else {
-    isActiveChapter.value = null
-  }
+    if (isActiveChapter.value !== id) {
+        isActiveChapter.value = id
+    } else {
+        isActiveChapter.value = null
+    }
 }
 
 const handleClickQuestion = (id) => {
-  if(isActiveQuestion.value.question !== id) {
-    isActiveQuestion.value.question = id
-    isActiveQuestion.value.chapter = isActiveChapter.value
-    isActiveQuestion.value.answer = chapters.value[isActiveChapter.value].items[isActiveQuestion.value.question].answer
-    isActiveQuestion.value.title = chapters.value[isActiveChapter.value].items[isActiveQuestion.value.question].question
+    if (isActiveQuestion.value.question !== id) {
+        isActiveQuestion.value.question = id
+        isActiveQuestion.value.chapter = isActiveChapter.value
+        isActiveQuestion.value.answer = chapters.value[isActiveChapter.value].items[isActiveQuestion.value.question].answer
+        isActiveQuestion.value.title = chapters.value[isActiveChapter.value].items[isActiveQuestion.value.question].question
 
-    // скролл на начало страницы при выборе вопроса
-    if (helpContent.value) {
-      const rect = helpContent.value.getBoundingClientRect();
-      const offset = window.scrollY + rect.top - settingStore.headerHeight;
-      smoothScroll(offset);
+        // скролл на начало страницы при выборе вопроса
+        if (helpContent.value) {
+            const rect = helpContent.value.getBoundingClientRect();
+            const offset = window.scrollY + rect.top - settingStore.headerHeight;
+            smoothScroll(offset);
+        }
+
+        router.push({ query: { question: isActiveQuestion.value.question, chapter: isActiveQuestion.value.chapter } })
     }
-
-    router.push({query: {question: isActiveQuestion.value.question, chapter: isActiveQuestion.value.chapter}})
-  }
 
 }
 
 onMounted(() => {
-  settingStore.getHelps().then(res => {
-    if(res) {
-      chapters.value = res
+    settingStore.getHelps().then(res => {
+        if (res) {
+            chapters.value = res
 
-      if(router.currentRoute.value.query.chapter && router.currentRoute.value.query.question) {
-        isActiveChapter.value = Number(router.currentRoute.value.query.chapter)
-        isActiveQuestion.value.question = Number(router.currentRoute.value.query.question)
-        isActiveQuestion.value.answer = chapters.value[isActiveChapter.value].items[isActiveQuestion.value.question].answer
-        isActiveQuestion.value.title = chapters.value[isActiveChapter.value].items[isActiveQuestion.value.question].question
-      } else {
-        isActiveQuestion.value.answer = chapters.value[isActiveQuestion.value.chapter].items[isActiveQuestion.value.question].answer
-        isActiveQuestion.value.title = chapters.value[isActiveQuestion.value.chapter].items[isActiveQuestion.value.question].question
-      }
+            if (router.currentRoute.value.query.chapter && router.currentRoute.value.query.question) {
+                isActiveChapter.value = Number(router.currentRoute.value.query.chapter)
+                isActiveQuestion.value.question = Number(router.currentRoute.value.query.question)
+                isActiveQuestion.value.answer = chapters.value[isActiveChapter.value].items[isActiveQuestion.value.question].answer
+                isActiveQuestion.value.title = chapters.value[isActiveChapter.value].items[isActiveQuestion.value.question].question
+            } else {
+                isActiveQuestion.value.answer = chapters.value[isActiveQuestion.value.chapter].items[isActiveQuestion.value.question].answer
+                isActiveQuestion.value.title = chapters.value[isActiveQuestion.value.chapter].items[isActiveQuestion.value.question].question
+            }
 
-      handleClickQuestion(isActiveQuestion.value.question)
-    }
-  })
+            handleClickQuestion(isActiveQuestion.value.question)
+
+            // Генерация JSON-LD schema после загрузки данных
+            const schema = generateFAQSchema();
+            if (schema) {
+                useHead({
+                    script: [
+                        {
+                            type: 'application/ld+json',
+                            children: JSON.stringify(schema)
+                        }
+                    ]
+                });
+            }
+        }
+    })
 })
 
 </script>
 
 <style lang="scss">
-
 .help-container {
-  display: flex;
-  font-size: 1.6rem;
-  column-gap: 2em;
+    display: flex;
+    font-size: 1.6rem;
+    column-gap: 2em;
 
-  &__left-side {
-    flex: 0 0 27%;
-    max-width: 27%;
-  }
-
-  &__questions {
-    position: sticky;
-  }
-
-  &__question-btn {
-    font-size: 1em;
-    justify-content: space-between;
-    border-radius: 0;
-    width: 100%;
-    color: var(--primary-color);
-
-    svg {
-      transform: rotate(-90deg);
-      flex-shrink: 0;
+    &__left-side {
+        flex: 0 0 27%;
+        max-width: 27%;
     }
 
-    &:hover {
-      background-color: var(--primary-color);
-      color: #fff !important;
+    &__questions {
+        position: sticky;
+    }
 
-      svg {
-        path {
-          fill: #fff;
+    &__question-btn {
+        font-size: 1em;
+        justify-content: space-between;
+        border-radius: 0;
+        width: 100%;
+        color: var(--primary-color);
+
+        svg {
+            transform: rotate(-90deg);
+            flex-shrink: 0;
         }
-      }
-    }
-  }
 
-  .active {
-    svg {
-      transform: rotate(0);
-    }
-  }
+        &:hover {
+            background-color: var(--primary-color);
+            color: #fff !important;
 
-  &__question-list {
-    font-family: 'fira-sans', sans-serif;
-    font-size: .875em;
-    padding-block: .75em;
-
-    .btn {
-      text-wrap: wrap;
-      text-align: left;
+            svg {
+                path {
+                    fill: #fff;
+                }
+            }
+        }
     }
 
     .active {
-      color: var(--primary-color);
-    }
-  }
-
-  &__content {
-    flex: 0 1 50%;
-
-    h4 {
-      font-size: 1.5em;
-      margin-block: 1.25em .83em;
-    }
-
-    a {
-        display: inline;
-        color: var(--primary-color);
-
-        &:hover {
-            text-decoration: underline;
+        svg {
+            transform: rotate(0);
         }
     }
-  }
 
-  &__question-btn {
-    text-wrap: wrap;
-    text-align: left;
-  }
+    &__question-list {
+        font-family: 'fira-sans', sans-serif;
+        font-size: .875em;
+        padding-block: .75em;
+
+        .btn {
+            text-wrap: wrap;
+            text-align: left;
+        }
+
+        .active {
+            color: var(--primary-color);
+        }
+    }
+
+    &__content {
+        flex: 0 1 50%;
+
+        h4 {
+            font-size: 1.5em;
+            margin-block: 1.25em .83em;
+        }
+
+        a {
+            display: inline;
+            color: var(--primary-color);
+
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+    }
+
+    &__question-btn {
+        text-wrap: wrap;
+        text-align: left;
+    }
 }
 
 .attachment--preview {
-  max-width: 100%;
-  text-align: center;
-  border: 2px solid #dee2e6;
-  cursor: pointer;
-  margin: 0 0 1em;
-
-  img {
     max-width: 100%;
-    width: auto;
-  }
+    text-align: center;
+    border: 2px solid #dee2e6;
+    cursor: pointer;
+    margin: 0 0 1em;
+
+    img {
+        max-width: 100%;
+        width: auto;
+    }
 }
 
 figure.attachment figcaption {
-  display: none;
+    display: none;
 }
 
 @include tablet {
-  .help-container {
-    &__content {
-      flex-grow: 1;
-    }
+    .help-container {
+        &__content {
+            flex-grow: 1;
+        }
 
-    &__left-side {
-      flex: 0 0 30%;
-      max-width: 30%;
-    }
+        &__left-side {
+            flex: 0 0 30%;
+            max-width: 30%;
+        }
 
-    &__question-btn {
-      font-size: 16px;
-    }
+        &__question-btn {
+            font-size: 16px;
+        }
 
-    &__question-list {
-      font-size: 1.6rem;
+        &__question-list {
+            font-size: 1.6rem;
+        }
     }
-  }
 }
 
 @include mobile {
-  .help-container {
-    flex-direction: column;
+    .help-container {
+        flex-direction: column;
 
-    &__left-side {
-      flex-basis: 100%;
-      max-width: 100%;
-    }
+        &__left-side {
+            flex-basis: 100%;
+            max-width: 100%;
+        }
 
-    &__question-btn {
-      font-size: 1.4rem;
-    }
+        &__question-btn {
+            font-size: 1.4rem;
+        }
 
-    &__question-list {
-      font-size: 1.4rem;
-    }
+        &__question-list {
+            font-size: 1.4rem;
+        }
 
-    &__content {
-      font-size: 1.4rem;
-      h4 {
-        font-size: 1.6rem;
-      }
+        &__content {
+            font-size: 1.4rem;
+
+            h4 {
+                font-size: 1.6rem;
+            }
+        }
     }
-  }
 }
-
 </style>
